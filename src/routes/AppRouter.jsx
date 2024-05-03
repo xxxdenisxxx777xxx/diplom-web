ecefb7e2ba62dab3e29999bf437980a5abeaa50d
xxxdenisxxx777xxx <xxxdenisxxx333xxx@gmail.com>
2024-05-03 16:42:40 +0300

    repair router

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -6,7 +6,7 @@ import Login from "../Login";
 const AppRouter = () => {
     return (
         <Routes>
-            <Route path="/login" index element={<Login />} />
+            <Route path="/" index element={<Login />} />
             <Route path="/adminpanel" element={<BasePage />} />
 
         </Routes>
